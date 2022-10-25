c0709a874190fc6ed94f82051b98487ef3884b44
Taylor <taylorlacortsuporte@gmail.com>
2022-10-25 17:49:01 -0300

    estudos_25_10

---
+++ controle/template_string.js
@@ -0,0 +1,11 @@
+const a = 10
+const b = 11
+
+console.log (`A = ${a} B = ${b}`) // FOI UTILIZADO O ` PARA CONSEGUIR INSERIR TEXTO E NUMEROS
+// PARA ADICIONAR O VALOR DA STRING FOI UTILIZADO ${}
+
+console.log(`1 + 2 = ${1+2}`)
+
+console.log(`Valor gerado foi : ${Math.random()}`)
+
+
