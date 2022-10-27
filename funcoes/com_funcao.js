9a729742d6cef5be12d083e002f1d0389afa1e70
Taylor <taylorlacortsuporte@gmail.com>
2022-10-27 15:02:05 -0300

    estudos_27_10

---
+++ funcoes/com_funcao.js
@@ -0,0 +1,31 @@
+const d1 = 3;
+const m1 = 7;
+const a1 = 2021;
+
+function Exibir_Data1() {
+
+    console.log(`${d1}/${m1}/${a1}`)
+}
+const d2 = 26;
+const m2 = 10;
+const a2 = 2022;
+
+function Exibir_Data2() {
+    console.log(`${d2}/${m2}/${a2}`)
+}
+
+Exibir_Data1();
+Exibir_Data1();
+Exibir_Data1();
+
+
+
+
+Exibir_Data2();
+Exibir_Data2();
+Exibir_Data2();
+
+
+
+//finction - primeiro passo é definir qual o nome da funcção exemplo : function somar 
+// Os parametros do parenteses recebem uma função exemplo (a , b) {  dentro das chaves ficam os blocos de código }
---
+++ funcoes/com_funcao_2.js
@@ -0,0 +1,28 @@
+const d1 = 03;
+const m1 = 07;
+const a1 = 2021;
+
+
+const d2 = 26;
+const m2 = 10;
+const a2 = 2022;
+
+
+function Exibir_Data(d, m, a) {
+    console.log(`${d}/${m}/${a}`)
+}
+
+Exibir_Data(d1, m1, a1);
+Exibir_Data(d1, m1, a1);
+Exibir_Data(d1, m1, a1);
+
+
+Exibir_Data(d2, m2, a2);
+Exibir_Data(d2, m2, a2);
+Exibir_Data(d2, m2, a2);
+
+
+
+
+
+
