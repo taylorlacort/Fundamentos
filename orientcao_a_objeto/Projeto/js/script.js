7d693409537042169b7aef677d48d0c9ecbfa579
Taylor <taylorlacortsuporte@gmail.com>
2022-11-22 17:01:56 -0300

    projeto_22_11

--- orientcao_a_objeto/Projeto/js/script.js
+++ orientcao_a_objeto/Projeto/js/script.js
@@ -1,1 +1,24 @@
-console.log('Deu certo!!')
+class Calculator {
+    constructor() {
+        this.upperValue = document.querySelector('#upper-number');
+        this.resultValue = document.querySelector('#result-number')
+        this.reset = 0;
+    }
+    btnPress() {
+        console.log(this)
+    }
+}
+
+//start obj
+let calc = new Calculator;
+
+//start btns
+
+let buttons = document.querySelectorAll('.btn');
+
+//map all buttons
+
+for (let i = 0; buttons.length > i; i++) {
+    buttons[i].addEventListener('click',calc.btnPress)
+}
+
