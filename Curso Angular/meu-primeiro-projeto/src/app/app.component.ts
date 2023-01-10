ae4a6ab4c7a08dc752e3364fd6593d37e6cd6990
Taylor <taylorlacortsuporte@gmail.com>
2023-01-10 17:50:09 -0300

    curso_angular

--- Curso Angular/meu-primeiro-projeto/src/app/app.component.ts
+++ Curso Angular/meu-primeiro-projeto/src/app/app.component.ts
@@ -1,10 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss'],
 })
-export class AppComponent {
-  title = 'meu-primeiro-projeto';
+
+export class Homecomponet implements OnInit {
+  constructor() { }
+  ngOnInit() { }
 }
--- Curso Angular/meu-primeiro-projeto/src/app/app.module.ts
+++ Curso Angular/meu-primeiro-projeto/src/app/app.module.ts
@@ -2,17 +2,21 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { TitleComponent } from './title/title.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+
+
+    
+    TitleComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: []
 })
 export class AppModule { }
