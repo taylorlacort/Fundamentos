ba1e73c320081016d135cdf635622bd04ffa7ad9
Taylor <taylorlacortsuporte@gmail.com>
2023-01-12 17:11:13 -0300

    hack_angular

--- Curso_angular/meu_primeiro_progeto/src/app/app.component.ts
+++ Curso_angular/meu_primeiro_progeto/src/app/app.component.ts
@@ -2,9 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  template: '1<router-outlet></router-outlet>'
 })
 export class AppComponent {
-  title = 'meu_primeiro_progeto';
+
 }
